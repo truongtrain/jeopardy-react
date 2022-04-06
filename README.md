ed4c61a5f00926dcae3b0cd4d15ef7d83f729e14
truongtrain <alan.truong@utah.edu>
2022-04-05 20:26:56 -0600

    create board

--- README.md
+++ README.md
@@ -68,3 +68,21 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+Jeopardy % Correct Per Response => Estimated Odds of buzzing in first when contested:
+
+Round 1
+$200: 6.3 / 7.3 = 0.863 => 0.333
+$400: 3.2 / 4.2 = 0.762 => 0.377
+$600: 3 / 4 = 0.750 => 0.383
+$800: 2.2 / 3.2 = 0.688 => 0.418
+$1000: 1.2 / 2.2 = 0.545 => 0.500
+
+Round 2
+$400: 4.6 / 5.6 = 0.821 => 0.350
+$800: 2.3 / 3.3 = 0.697 => 0.412
+$1200: 1.9 / 2.9 = 0.655 => 0.438
+$1600: 1.3 / 2.3 = 0.565 => 0.500
+$2000: 0.8 / 1.8 = 0.444 => 0.500
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import showData from './jeopardy.json'
 
-function App() {
+console.log(showData);
+
+
+const App = () => {
+  let visibleMatrix = [];
+  for (let row = 0; row < 5; row++) {
+    visibleMatrix.push([]);
+    for (let col = 0; col < 6; col++) {
+      visibleMatrix[row].push(false);
+    }
+  }
+  let [visible, setVisible] = useState(visibleMatrix);
+  let contestants = showData.contestants.filter(
+    contestant => contestant !== showData.weakest_contestant
+  );
+  contestants.push('Alan');
+  let scores = {};
+  contestants.forEach(contestant => scores[contestant] = 0);
+
+  function displayClue(row, column) {
+    let visibleCopy = [...visible];
+    if (visibleCopy[row][column] !== undefined) {
+      visibleCopy[row][column] = true;
+      setVisible(visibleCopy);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>
+        <div>{contestants[0]}: ${scores[contestants[0]]}</div>
+        <div>{contestants[1]}: ${scores[contestants[1]]}</div>
+        <div>{contestants[2]}: ${scores[contestants[2]]}</div>
+      </h3>
+      <br></br><br></br><br></br>
+      <table>
+        <thead>
+          <tr>
+            <th>{showData.jeopardy_round[0][0].category}</th>
+            <th>{showData.jeopardy_round[1][0].category}</th>
+            <th>{showData.jeopardy_round[2][0].category}</th>
+            <th>{showData.jeopardy_round[3][0].category}</th>
+            <th>{showData.jeopardy_round[4][0].category}</th>
+            <th>{showData.jeopardy_round[5][0].category}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(Array(5), (arrayElement, row) => {
+            return (<tr key={row}>
+            {showData.jeopardy_round.map((round, column) => {
+              return (
+              <td key={column}>
+                <span>{visible[row][column] && round[row].text}</span>
+                {
+                  !visible[row][column] && <button onClick={() => displayClue(row, column)}>
+                  {round[row].value}
+                  </button>
+                }
+              </td>)})}
+          </tr>)
+          })}
+          {/* <tr>
+            <td>{showData.jeopardy_round[0][1].text}</td>
+            <td>{showData.jeopardy_round[1][1].text}</td>
+            <td>{showData.jeopardy_round[2][1].text}</td>
+            <td>{showData.jeopardy_round[3][1].text}</td>
+            <td>{showData.jeopardy_round[4][1].text}</td>
+            <td>{showData.jeopardy_round[5][1].text}</td>
+          </tr>
+          <tr>
+            <td>{showData.jeopardy_round[0][2].text}</td>
+            <td>{showData.jeopardy_round[1][2].text}</td>
+            <td>{showData.jeopardy_round[2][2].text}</td>
+            <td>{showData.jeopardy_round[3][2].text}</td>
+            <td>{showData.jeopardy_round[4][2].text}</td>
+            <td>{showData.jeopardy_round[5][2].text}</td>
+          </tr>
+          <tr>
+            <td>{showData.jeopardy_round[0][3].text}</td>
+            <td>{showData.jeopardy_round[1][3].text}</td>
+            <td>{showData.jeopardy_round[2][3].text}</td>
+            <td>{showData.jeopardy_round[3][3].text}</td>
+            <td>{showData.jeopardy_round[4][3].text}</td>
+            <td>{showData.jeopardy_round[5][3].text}</td>
+          </tr>
+          <tr>
+            <td>{showData.jeopardy_round[0][4].text}</td>
+            <td>{showData.jeopardy_round[1][4].text}</td>
+            <td>{showData.jeopardy_round[2][4].text}</td>
+            <td>{showData.jeopardy_round[3][4].text}</td>
+            <td>{showData.jeopardy_round[4][4].text}</td>
+            <td>{showData.jeopardy_round[5][4].text}</td>
+          </tr> */}
+        </tbody>
+      </table>
     </div>
   );
 }
