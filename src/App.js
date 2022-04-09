6e36ed5279917e1567d87b9e8ddcba98daa8913e
truongtrain <alan.truong@utah.edu>
2022-04-08 23:35:05 -0600

    create banner component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import './App.css';
 import React, {useState} from 'react';
-import showData from './jeopardy.json'
+import showData from './jeopardy.json';
+import Banner from './Banner';
 
 console.log(showData);
-const App = () => {
-  let visibleMatrix = [];
-  for (let row = 0; row < 5; row++) {
-    visibleMatrix.push([]);
-    for (let col = 0; col < 6; col++) {
-      visibleMatrix[row].push(false);
-    }
+let visibleMatrix = [];
+for (let row = 0; row < 5; row++) {
+  visibleMatrix.push([]);
+  for (let col = 0; col < 6; col++) {
+    visibleMatrix[row].push(false);
   }
+}
+let contestants = showData.contestants.filter(
+  contestant => contestant !== showData.weakest_contestant
+);
+contestants.push('Alan');
+  
+let clueNumber = 1;
+// setTimeout(() => findClue(clueNumber), 3000);  
+
+  
+
+const App = () => {
+  
   let [visible, setVisible] = useState(visibleMatrix);
-  let [message, setMessage] = useState('');
+  
   let [board, setBoard] = useState(showData.jeopardy_round);
   let [tableStyle, setTableStyle] = useState('table-light-off');
-  let contestants = showData.contestants.filter(
-    contestant => contestant !== showData.weakest_contestant
-  );
-  // useEffect(() => turnOffLight(), [message]);
-
-  contestants.push('Alan');
-  let scores = {};
-  contestants.forEach(contestant => scores[contestant] = 0);
-  let clueNumber = 1;
-  setTimeout(() => findClue(clueNumber), 3000);  
 
   function findClue(clueNumber) {
     let visibleCopy = [...visible];
@@ -32,8 +34,8 @@ const App = () => {
       for (let row = 0; row < 5; row++) {
         if (showData.jeopardy_round[col][row].number === clueNumber) {
           const clue = showData.jeopardy_round[col][row];
-          message = clue.category + ' for $' + clue.value;
-          setMessage(message);
+          // message = clue.category + ' for $' + clue.value;
+          // setMessage(message);
           visibleCopy[row][col] = true;
           setTimeout(() => showClue(visibleCopy, row, col), 2000);
           return;
@@ -42,16 +44,15 @@ const App = () => {
     }
   }
 
+  function turnOffLight() {
+    setTableStyle('table-light-off');
+  }
+
   function showClue(visibleCopy, row, col) {
     setVisible(visibleCopy);
     const clue = showData.jeopardy_round[col][row];
     const charsPerSecond = 16;
     setTimeout(() => clearClue(row, col), 1000*clue.text.length/charsPerSecond);
-  }
-
-  function turnOffLight() {
-    setMessage('Alan');
-    setTableStyle('table-light-off');
   }
 
   function clearClue(row, col) {
@@ -71,16 +72,7 @@ const App = () => {
 
   return (
     <div>
-      <div className='banner'>
-        <h3>
-        <span className='message'>{message}</span>
-        <br></br>
-        <button type='button' className='answer-button-2' onClick={turnOffLight}>Answer!</button>
-          <div>${scores[contestants[0]]}<br></br>{contestants[0]}</div>
-          <div>${scores[contestants[1]]}<br></br>{contestants[1]}</div>
-          <div>${scores[contestants[2]]}<br></br>{contestants[2]}</div>
-        </h3>
-      </div>
+      <Banner contestants={contestants} />
       <table className={tableStyle}>
         <thead>
           <tr>
