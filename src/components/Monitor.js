fe5df65532523d10252dba5ca77b47c7a4fbd334
truongtrain <alan.truong@utah.edu>
2023-04-28 23:27:47 -0600

    refactor directory structure

---
+++ src/components/Monitor.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Message(props) {
+    const message = props.message;
+    const imageUrl = props.imageUrl;
+
+    return (
+        <div id='monitor'>
+            {imageUrl === 'logo' && <img src={require('../resources/jeopardy_logo.jpeg')} alt=""></img>}
+            {imageUrl && imageUrl !== 'logo' && <img src={imageUrl} alt=""></img>}  
+            <div>
+                <div>{message.line1}</div>
+                <div>{message.line2}</div>      
+            </div>                           
+        </div>
+    );
+}
+
+export default Message;
---
+++ src/components/Podium.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import Timeout from '../resources/timeout.mp3';
+
+function Podium(props) {
+    const contestants = props.contestants;
+    const responseCountdownIsActive = props.startTimer;
+    const playerName = props.playerName;
+    const names = Object.keys(contestants);
+    let [ticks, setTicks] = useState(new Array(9).fill(true));
+
+    // 5 second timer to respond after my name is called
+    useEffect(() => {
+        let responseCountdownInterval = {};
+        let timeout = new Audio(Timeout);
+        let responseCountdown = 4;
+        if (responseCountdownIsActive) {
+            responseCountdownInterval = setInterval(() => {
+                if (responseCountdown === 0) {
+                    clearInterval(responseCountdownInterval);
+                    timeout.play();
+                } else {
+                    setTicks(new Array(responseCountdown * 2 - 1).fill(true));
+                }
+                responseCountdown -= 1;
+            }, 1000);
+        } else {
+            setTicks(new Array(9).fill(true));
+        }
+        return () => clearInterval(responseCountdownInterval);
+    }, [responseCountdownIsActive]);
+
+    return (
+        <div id='podiums'>
+            {names.map(name => {
+                return contestants[name] && <div className='podium' key={name}>
+                    {name === playerName && responseCountdownIsActive && 
+                    <div className='tick-row'>
+                        {ticks.map((_tick, index) =>
+                            <span key={'tick' + index} className='tick'></span>
+                        )}
+                    </div>}
+                    {!(name === playerName && responseCountdownIsActive) && <div className='tick-row'></div>}
+                    <div className='podium-row'>${contestants[name].score}</div>
+                    <div className='podium-row name-row'>{name}</div>
+                    <div className='big-podium-row'>{contestants[name].response}</div>
+                    {contestants[name].response && <div className='podium-row'>${contestants[name].wager}</div>}
+                </div>
+            })}
+        </div>
+    );
+}
+
+export default Podium;
